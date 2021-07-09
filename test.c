764022b56703d41272caeeb4c301ff943d71e811
sadrasabouri <sabouri.sadra@gmail.com>
2021-07-09 19:26:44 +0430

    edit : general refactor.

--- test.c
+++ test.c
@@ -12,7 +12,9 @@ float derivative = 0;
 float p_error = 0;
 float k_p, k_i, k_d;
 
-void set_float_values(int k_p_in, int k_i_in, int k_d_in) {
+
+void set_float_values(int k_p_in, int k_i_in, int k_d_in)
+{
    k_p = (float) k_p_in;
    k_p /= 16.0;
    k_i = (float) k_i_in;
@@ -21,8 +23,10 @@ void set_float_values(int k_p_in, int k_i_in, int k_d_in) {
    k_d /= 16.0;
 }
 
-void report_output(float value) {
-   float val = value * 100; //   0 < val < 256
+
+void report_output(float value)
+{
+   float val = value * 100;               //   0 < val < 256
    //val += 256;
    //val /= 2; //   0 < val < 256
    int out = (int) val;
@@ -30,7 +34,9 @@ void report_output(float value) {
    printf("OUT(int) = %d ***** OUT(float) = %f\n", out, value);
 }
 
-float calculate(float set_point, float pv) {
+
+float calculate(float set_point, float pv)
+{
    float error = set_point;
    error -= pv;
    float p_out, i_out, d_out;
@@ -38,7 +44,7 @@ float calculate(float set_point, float pv) {
    integral += error;
    i_out = k_i * integral / SCALE;
    derivative = error - p_error;
-   d_out = k_d * derivative * SCALE; // divided by 0.02
+   d_out = k_d * derivative * SCALE;      // divided by 0.02
    float output = p_out + d_out + i_out;
    if(output > MAX)
 	 output = MAX;
@@ -47,6 +53,8 @@ float calculate(float set_point, float pv) {
    p_error = error;
    return output;
 }
+
+
 int main()
 {
    int k_p = 1;
@@ -58,11 +66,13 @@ int main()
    set_float_values(k_p, k_i, k_d);
    
    //DDRC = 0xFF;
-   while (1) {
+   while (1)
+   {
       pv = calculate(set_point, pv);
       report_output(pv);
       //_delay_ms(20);
       Sleep(250);
    }
+   
    return 0;
 }
